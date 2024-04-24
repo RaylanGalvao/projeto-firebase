de217cad392a7174c9263d5b693d4129ecf8012c
RaylanGalvao <153932047+RaylanGalvao@users.noreply.github.com>
2024-04-24 09:45:24 -0300

    mais atualizações

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
+import { useState } from "react";
 import { db } from "./firebaseconection";
+import { doc, setDoc, collection, addDoc } from 'firebase/firestore'
 
-db
+import './app.css'
 
 function App() {
+
+  const [titulo, setTitulo] = useState()
+  const [autor, setAutor] = useState()
+
+  async function handleAdd(){
+    await setDoc(doc(db, "posts", "12345"),{
+      titulo: titulo,
+      autor: autor,
+    })
+    .then(()=>{
+      console.log("dados registrados no banco")
+    })
+    .catch((error)=>{
+      console.log("gerou erro"+ error)
+    })
+  }
+
   return (
     <div>
       <h1>ReactJS + firebase</h1>
+
+      <div className="conteiner">
+      <label>Titulo:</label>
+      <textarea 
+      type="text"
+      placeholder="Digite o tiulo"
+      value={titulo}
+      onChange={ (e) => setTitulo(e.target.value)}
+      />
+      <label>Autor:</label>
+      <input
+      type="text"
+      placeholder="Autor do post"
+      value={autor}
+      onChange={(e) => setAutor(e.target.value)}
+      />
+      <button onClick={handleAdd}>Cadatrar</button>
+      </div>
     </div>
   )
 }
