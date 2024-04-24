a44fa8565d34cd58fcee2f2225bc2e4c7a2202a2
RaylanGalvao <153932047+RaylanGalvao@users.noreply.github.com>
2024-04-24 08:47:28 -0300

    primeiro contato com firebase

---
+++ src/firebaseconection.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyALidht8jvl-4ENHkF7iCxIkQatqcbIEsA",
+    authDomain: "curso-837bb.firebaseapp.com",
+    projectId: "curso-837bb",
+    storageBucket: "curso-837bb.appspot.com",
+    messagingSenderId: "686047411594",
+    appId: "1:686047411594:web:b32996e73ec462e1e85c73",
+    measurementId: "G-1EFLM4PVC6"
+  };
+
+const firebaseapp = initializeApp(firebaseConfig)
+const db = getFirestore(firebaseapp)
+
+export {db}
