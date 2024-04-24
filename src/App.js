2608f3c9f3444e24990b47d902a1c9c4a2cfcf57
RaylanGalvao <153932047+RaylanGalvao@users.noreply.github.com>
2024-04-24 10:41:37 -0300

    aprendendo mais sobre firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { db } from "./firebaseconection";
-import { doc, setDoc, collection, addDoc } from 'firebase/firestore'
+import { doc, setDoc, collection, addDoc, getDoc, getDocs } from 'firebase/firestore'
 
 import './app.css'
 
@@ -9,22 +9,67 @@ function App() {
   const [titulo, setTitulo] = useState()
   const [autor, setAutor] = useState()
 
+  const [posts, setPosts] = useState([])
+
   async function handleAdd(){
-    await setDoc(doc(db, "posts", "12345"),{
-      titulo: titulo,
-      autor: autor,
-    })
-    .then(()=>{
-      console.log("dados registrados no banco")
+  //   await setDoc(doc(db, "posts", "12345"),{
+  //     titulo: titulo,
+  //     autor: autor,
+  //   })
+  //   .then(()=>{
+  //     console.log("dados registrados no banco")
+  //   })
+  //   .catch((error)=>{
+  //     console.log("gerou erro"+ error)
+  //   })
+
+      await addDoc(collection(db, "posts"),{
+        titulo: titulo,
+        autor: autor,
+      })
+      .then(()=>{
+        console.log("CADASTRADO COM SUCESSO")
+        setAutor("")
+        setTitulo("")
+      })
+      .catch((error)=>{
+        console.log("ERRO"+error)
+      })
+  }
+
+
+  async function buscarPosts(){
+    // const postRef = doc(db, "posts", "5tWZLGqVzpTaJnJ4CNN9")
+    // await getDoc(postRef)
+    // .then((snapshot)=>{
+    //   setAutor(snapshot.data().autor)
+    //   setTitulo(snapshot.data().titulo)
+    // })
+    // .catch(()=>{
+    //   console.log("Erro ao buscar")
+    // })
+    const postRef = collection(db, "posts")
+    await getDocs(postRef)
+    .then((snapshot)=>{
+      let lista = []
+      snapshot.forEach((doc)=>{
+        lista.push({
+          id: doc.id,
+          titulo: doc.data().titulo,
+          autor: doc.data().autor,
+        })
+      })
+      setPosts(lista)
     })
     .catch((error)=>{
-      console.log("gerou erro"+ error)
+      console.log("Deu algum erro ao buscar")
     })
   }
 
+
   return (
     <div>
-      <h1>ReactJS + firebase</h1>
+      <h1>ReactJS + firebase :)</h1>
 
       <div className="conteiner">
       <label>Titulo:</label>
@@ -42,6 +87,18 @@ function App() {
       onChange={(e) => setAutor(e.target.value)}
       />
       <button onClick={handleAdd}>Cadatrar</button>
+      <button onClick={buscarPosts}>Buscar posts</button>
+
+      <ul>
+        {posts.map((post)=>{
+          return (
+            <li key={post.id}>
+              <span>titulo: {post.titulo} </span><br/>
+              <span>autor: {post.autor} </span><br/><br/>
+            </li>
+          )
+        })}
+      </ul>
       </div>
     </div>
   )
